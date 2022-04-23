7ffc23a38715d1b2df81819213c0a774c275b532
Angel Velasquez <pcisavel@upc.edu.pe>
2022-04-23 09:45:01 -0500

    feat (navigation): established navigation routes.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {HomeComponent} from "./public/home/home.component";
+import {AboutComponent} from "./public/about/about.component";
+import {StudentsComponent} from "./students/pages/students/students.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'about', component: AboutComponent },
+  { path: '', redirectTo: ' home', pathMatch: 'full' },
+  { path: 'students', component: StudentsComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
