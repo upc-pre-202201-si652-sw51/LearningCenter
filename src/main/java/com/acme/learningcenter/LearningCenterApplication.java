b373a19f586c4139a1507eda687a2ae1ad6eea8f
Angel Velasquez <pcisavel@upc.edu.pe>
2022-05-16 13:58:58 -0500

    Initial Commit

---
+++ src/main/java/com/acme/learningcenter/LearningCenterApplication.java
@@ -0,0 +1,13 @@
+package com.acme.learningcenter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearningCenterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LearningCenterApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/acme/learningcenter/LearningCenterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.acme.learningcenter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearningCenterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
