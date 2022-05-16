5762c8cd0e8fb2fbd5a0bc75ed64fa37a94741ae
Angel Velasquez <pcisavel@upc.edu.pe>
2022-05-16 14:19:42 -0500

    chore(audit): Implemented Abstract class with Audit Trail attributes.

---
+++ src/main/java/com/acme/learningcenter/shared/domain/model/AuditModel.java
@@ -0,0 +1,30 @@
+package com.acme.learningcenter.shared.domain.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Getter
+@Setter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+@JsonIgnoreProperties(value = {"createdAt", "updatedAt"}, allowGetters = true)
+public abstract class AuditModel {
+
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(name = "created_at", nullable = false, updatable = false)
+    @CreatedDate
+    private Date createdAt;
+
+    @Column(name = "updated_at", nullable = false)
+    @Temporal(TemporalType.TIMESTAMP)
+    @LastModifiedDate
+    private Date updatedAt;
+
+}
