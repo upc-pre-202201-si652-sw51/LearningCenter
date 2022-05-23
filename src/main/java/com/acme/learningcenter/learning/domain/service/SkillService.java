4cb3a28798a1254a36acdc7eccce16125b3736ad
Angel Velasquez <pcisavel@upc.edu.pe>
2022-05-23 13:47:57 -0500

    feat(skills): Implemented Service Layer Contract.

---
+++ src/main/java/com/acme/learningcenter/learning/domain/service/SkillService.java
@@ -0,0 +1,17 @@
+package com.acme.learningcenter.learning.domain.service;
+
+import com.acme.learningcenter.learning.domain.model.entity.Skill;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
+
+import java.util.List;
+
+public interface SkillService {
+  List<Skill> getAll();
+  Page<Skill> getAll(Pageable pageable);
+  Skill getById(Long skillId);
+  Skill create(Skill skill);
+  Skill update(Skill skill);
+  ResponseEntity<?> delete(Long skillId);
+}
