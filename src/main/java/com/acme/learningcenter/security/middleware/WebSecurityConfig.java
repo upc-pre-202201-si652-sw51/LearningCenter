d0ea709b8e1a9d86390a1557388e93f3a084b1fe
Angel Velasquez <pcisavel@upc.edu.pe>
2022-06-18 11:32:19 -0500

    feat(security): Added Security Configuration Class.

---
+++ src/main/java/com/acme/learningcenter/security/middleware/WebSecurityConfig.java
@@ -0,0 +1,51 @@
+package com.acme.learningcenter.security.middleware;
+
+import com.acme.learningcenter.security.domain.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+@Configuration
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+  @Autowired
+  UserService userService;
+
+  JwtAuthenticationEntryPoint unauthorizedHandler;
+
+  @Bean
+  public JwtAuthorizationFilter authorizationFilter() { return new JwtAuthorizationFilter(); }
+
+  @Bean
+  public PasswordEncoder passwordEncoder() { return new BCryptPasswordEncoder(); }
+
+  @Override
+  protected void configure(AuthenticationManagerBuilder builder) throws Exception {
+    builder.userDetailsService(userService);
+  }
+
+  @Override
+  protected void configure(HttpSecurity http) throws Exception {
+    http.cors().and().csrf().disable()
+      .exceptionHandling()
+      .authenticationEntryPoint(unauthorizedHandler).and()
+      .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
+      .authorizeHttpRequests().antMatchers(
+        "/api/v1/users/auth/*",
+        "/swagger-ui/**",
+        "/api-docs/**").permitAll()
+      .anyRequest().authenticated();
+    http.addFilterBefore(authorizationFilter(), UsernamePasswordAuthenticationFilter.class);
+  }
+}
